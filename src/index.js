e3482a00276732dcafb09047f2ff8ae35d2c80f8
Maro Cheon <cjsakfh89@gmail.com>
2020-08-31 22:15:29 +0900

    Counter example using Redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+number.innerText = 0;
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () => {
-  count++;
-  updateText();
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") count++;
+  if (action.type === "MINUS") count--;
+  return count;
 };
 
-const handleMinus = () => {
-  count--;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
+countStore.subscribe(() => {
+  number.innerText = countStore.getState();
+});
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
